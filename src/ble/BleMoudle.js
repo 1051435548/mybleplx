3e6448fee86705967b1d3b974bae594330c4716d
yangbo <1051435548@qq.com>
2019-11-21 16:36:03 +0800

    1. 扫描设备 2. 连接蓝牙 3. 写数据/读数据

--- src/ble/BleMoudle.js
+++ src/ble/BleMoudle.js
@@ -1,14 +1,264 @@
-import {
-    Platform,
-    Alert,
-} from 'react-native';
 import {BleManager} from 'react-native-ble-plx';
+import {Alert} from 'react-native';
 import {Buffer} from 'buffer';
 
 export default class BleModule {
 
     constructor() {
         this.manager = new BleManager();
+        this.isConnecting = false;
+    }
 
+    /**
+     * 停止搜索蓝牙
+     * */
+    stopScan() {
+        this.manager.stopDeviceScan();
+        console.log('停止搜索');
+    }
+
+    /**
+     * 卸载蓝牙管理器
+     * */
+    destroy() {
+        this.manager.destroy();
+    }
+
+    alert = (text) =>  {
+        Alert.alert('提示', text, [{
+            text: '确定', onPress: () => {
+            },
+        }]);
+    };
+
+    /**
+     * 连接蓝牙
+     */
+    connect(deviceId) {
+        console.log('正在连接的设备ID：' + deviceId);
+        this.isConnecting = true;
+        return new Promise((resolve, reject) => {
+            this.manager.connectToDevice(deviceId)
+                .then(device => {
+                    console.log('连接成功：设备名称' + device.name, ' 设备ID' + device.id);
+                    this.peripheralId = device.id;
+                    return device.discoverAllServicesAndCharacteristics();
+                })
+                .then(device => {
+                    return BleModule.fetchServicesAndCharacteristicsForDevice(device);
+                })
+                .then(services => {
+                    this.isConnecting = false;
+                    this.getUUID(services);
+                    resolve();
+                })
+                .catch(error => {
+                    this.isConnecting = false;
+                    console.log('连接失败:', error);
+                    reject(error);
+                });
+        });
+    }
+
+    /**
+     * 断开蓝牙
+     * */
+    disconnect() {
+        return new Promise((resolve, reject) => {
+            this.manager.cancelDeviceConnection(this.peripheralId)
+                .then(res => {
+                    console.log('disconnect success', res);
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                    console.log('disconnect fail', err);
+                });
+        });
+    }
+
+    /**
+     * 获取蓝牙UUID
+     * @param device
+     * @returns {Promise<void>}
+     */
+    static async fetchServicesAndCharacteristicsForDevice(device) {
+        let servicesMap = {};
+        let services = await device.services();
+
+        for (let service of services) {
+            let characteristicsMap = {};
+            let characteristics = await service.characteristics();
+
+            for (let characteristic of characteristics) {
+                characteristicsMap[characteristic.uuid] = {
+                    uuid: characteristic.uuid,
+                    isReadable: characteristic.isReadable,
+                    isWritableWithResponse: characteristic.isWritableWithResponse,
+                    isWritableWithoutResponse: characteristic.isWritableWithResponse,
+                    isNotifiable: characteristic.isNotifiable,
+                    isNotifying: characteristic.isNotifying,
+                    value: characteristic.value,
+                };
+            }
+
+            servicesMap[service.uuid] = {
+                uuid: service.uuid,
+                isPrimary: service.isPrimary,
+                characteristicsMap: characteristics.length,
+                characteristics: characteristicsMap,
+            };
+        }
+        return servicesMap;
+    };
+
+    /**
+     * 初始化UUID
+     */
+    initUUID() {
+        // services
+        this.readServiceUUID = [];
+        this.writeWithResponseServiceUUID = [];
+        this.writeWithoutResponseServiceUUID = [];
+        this.nofityServiceUUID = [];
+        // characteristic
+        this.readCharacteristicUUID = [];
+        this.writeWithResponseCharacteristicUUID = [];
+        this.writeWithoutResponseCharacteristicUUID = [];
+        this.nofityCharacteristicUUID = [];
+    }
+
+    /**
+     * 获取Notify、Read、Write、WriteWithoutResponse的serviceUUID和characteristicUUID
+     */
+    getUUID(services) {
+        this.initUUID();
+        for (let i in services) {
+            //services里面包含了很多个characteristics
+            let characteristic = services[i].characteristics;
+            for (let j in characteristic) {
+                //characteristic里面包含isNotifiable、isNotifying、isReadable、isWritableWithResponse、isWritableWithoutResponse、uuid、value
+                if (characteristic[j].isReadable) {
+                    this.readServiceUUID.push(services[i].uuid);
+                    this.readCharacteristicUUID.push(characteristic[j].uuid);
+                }
+                if (characteristic[j].isWritableWithResponse) {
+                    this.writeWithResponseServiceUUID.push(services[i].uuid);
+                    this.writeWithResponseCharacteristicUUID.push(characteristic[j].uuid);
+                }
+                if (characteristic[j].isWritableWithoutResponse) {
+                    this.writeWithoutResponseServiceUUID.push(services[i].uuid);
+                    this.writeWithoutResponseCharacteristicUUID.push(characteristic[j].uuid);
+                }
+                if (characteristic[j].isNotifiable) {
+                    this.nofityServiceUUID.push(services[i].uuid);
+                    this.nofityCharacteristicUUID.push(characteristic[j].uuid);
+                }
+            }
+        }
+    }
+
+    /**
+     * 写数据
+     */
+    write(value, index) {
+        let formatValue;
+        if (value === '0D0A') {
+            formatValue = value;
+        } else {
+            formatValue = new Buffer(value, 'base64').toString('ascii');
+        }
+        let transactionId = 'write';
+        return new Promise((resolve, reject) => {
+            this.manager.writeCharacteristicWithResponseForDevice(
+                this.peripheralId,
+                this.writeWithResponseServiceUUID[index],
+                this.writeWithResponseCharacteristicUUID[index],
+                formatValue,
+                transactionId,
+            ).then(characteristic => {
+                resolve(characteristic);
+            }).catch(error => {
+                this.alert('write fail: ', error.reason);
+                reject(error);
+            });
+        });
+    }
+
+    /**
+     * 写数据 withoutResponse
+     */
+    writeWithoutResponse(value, index) {
+        let formatValue;
+        if (value === '0D0A') {
+            formatValue = value;
+        } else {
+            formatValue = new Buffer(value, 'base64').toString('ascii');
+        }
+        let transactionId = 'writeWithoutResponse';
+        return new Promise((resolve, reject) => {
+            this.manager.writeCharacteristicWithoutResponseForDevice(
+                this.peripheralId,
+                this.writeWithoutResponseServiceUUID[index],
+                this.writeWithoutResponseCharacteristicUUID[index],
+                formatValue,
+                transactionId,
+            ).then(characteristic => {
+                resolve(characteristic);
+            }).catch(error => {
+                this.alert('writeWithoutResponse fail: ',error.reason);
+                reject(error);
+            });
+        });
+    };
+
+    /**
+     * 读取数据
+     * @param index
+     * @returns {Promise<any> | Promise<*>}
+     */
+    read(index) {
+        return new Promise((resolve, reject) => {
+            this.manager.readCharacteristicForDevice(
+                this.peripheralId,
+                this.readServiceUUID[index],
+                this.readCharacteristicUUID[index],
+            ).then(characteristic => {
+                let buffer = Buffer.from(characteristic.value, 'base64');
+                const value = BleModule.byteToString(buffer);
+                resolve(value);
+            }).catch(error => {
+                this.alert('read fail: ' + error.reason);
+                reject(error);
+            });
+        });
+    };
+
+
+    /**
+     * byte数组转换成字符串
+     */
+    static byteToString(arr) {
+        if (typeof arr === 'string') {
+            return arr;
+        }
+        let str = '',
+            _arr = arr;
+        for (let i = 0; i < _arr.length; i++) {
+            let one = _arr[i].toString(2),
+                v = one.match(/^1+?(?=0)/);
+            if (v && one.length === 8) {
+                let bytesLength = v[0].length;
+                let store = _arr[i].toString(2).slice(7 - bytesLength);
+                for (let st = 1; st < bytesLength; st++) {
+                    store += _arr[st + i].toString(2).slice(2);
+                }
+                str += String.fromCharCode(parseInt(store, 2));
+                i += bytesLength - 1;
+            } else {
+                str += String.fromCharCode(_arr[i]);
+            }
+        }
+        return str;
     }
 }
--- src/ble/index.js
+++ src/ble/index.js
@@ -1,47 +1,417 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View, Platform} from 'react-native';
+import {Alert, FlatList, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import BleModule from './BleMoudle';
+import {ScanMode} from 'react-native-ble-plx';
 
+// 全局唯一的ble实例
 global.BluetoothManager = new BleModule();
 export default class BlePlx extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            scaning: false, //是否正在扫描蓝牙设备
+            devicesData: [], //搜到到的蓝牙设备
+            isConnected: false, //是否连接上设备
+            text: '',
+            writeData: '',
+            receiveData: '',
+            readData: '',
+            isMonitoring: false,
+        };
+        this.bluetoothReceiveData = [];
+        this.deviceMap = new Map(); //搜索到的蓝牙设备(将id作为键值存入保证不重复)
     }
 
     componentDidMount(): void {
 
         //监听系统蓝牙的变化
         this.onStateChangeListener = BluetoothManager.manager.onStateChange((state) => {
-            console.log("onStateChange: ", state);
-            if(state === 'PoweredOn'){
+            console.log('onStateChange: ', state);
+            if (state === 'PoweredOn') {
                 this.scan();
             }
         });
     }
 
     componentWillUnmount(): void {
+        BluetoothManager.destroy();
+        // 销毁组件时关掉所有监听
         this.onStateChangeListener && this.onStateChangeListener.remove();
+        this.monitorListener && this.monitorListener.remove();
+        this.disconnectListener && this.disconnectListener.remove();
     }
 
+
+    alert = (text) => {
+        Alert.alert('提示', text, [{
+            text: '确定', onPress: () => {
+            },
+        }]);
+    };
+
+    /**
+     * 打开手机蓝牙
+     */
+    accessOpenBlue = () => {
+        BluetoothManager.manager.enable()
+            .then(() => {
+                this.setState({scaning: false});
+                this.scan();
+                this.alert('蓝牙已连接');
+                console.log('系统请求打开蓝牙成功');
+            })
+            .catch(e => {
+                console.log('异常: ' + e);
+            });
+    };
 
     /**
      * 查找蓝牙设备
      */
     scan = () => {
-        console.log("开始查找设备");
+        if (!this.state.scaning) {
+            this.setState({scaning: true});
+            this.deviceMap.clear();
+            BluetoothManager.manager.startDeviceScan(null, {scanMode: ScanMode.LowLatency}, (error, device) => {
+                if (error) {
+                    //如果异常为[Device not authorized to use BluetoothLE] 则检查应用的位置权限是否打开
+                    console.log('startDeviceScan error:', error);
+                    if (error.errorCode === 102) {
+                        Alert.alert(
+                            '提示',
+                            '请打开手机蓝牙后再搜索',
+                            [{
+                                text: '确定',
+                                onPress: () => {
+                                    this.accessOpenBlue();
+                                },
+                            }]);
+                    }
+                    this.setState({scaning: false});
+                } else {
+                    this.deviceMap.set(device.id, device); //使用Map类型保存搜索到的蓝牙设备，确保列表不显示重复的设备
+                    this.setState({
+                        devicesData: [...this.deviceMap.values()],
+                    });
+                }
+            });
+
+            this.scanTimer && clearTimeout(this.scanTimer);
+            this.scanTimer = setTimeout(() => {
+                if (this.state.scaning) {
+                    BluetoothManager.stopScan();
+                    this.setState({scaning: false});
+                }
+            }, 5000);  //5秒后停止搜索
+
+        } else {
+            BluetoothManager.stopScan();
+            this.setState({scaning: false});
+        }
+    };
+
+    /**
+     * 断开蓝牙连接
+     */
+    disconnect = () => {
+        BluetoothManager.disconnect()
+            .then(response => {
+                this.setState({data: [...this.deviceMap.values()], isConnected: false});
+            })
+            .catch(error => {
+                this.setState({data: [...this.deviceMap.values()], isConnected: false});
+            });
+    };
+
+    /**
+     * 连接蓝牙设备
+     */
+    connect = (device) => {
+        if (this.state.scaning) {  //连接的时候正在扫描，先停止扫描
+            BluetoothManager.stopScan();
+            this.setState({scaning: false});
+        }
+        if (BluetoothManager.isConnecting) {
+            this.alert('当前蓝牙正在连接时不能打开另一个连接进程');
+            return;
+        }
+
+        let newData = [...this.deviceMap.values()];
+        newData[device.index].isConnecting = true;  //正在连接中
+        this.setState({devicesData: newData});
+
+        BluetoothManager.connect(device.item.id)
+            .then(() => {
+                newData[device.index].isConnecting = false;
+                this.setState({
+                    devicesData: [newData[device.index]],
+                    isConnected: true,
+                });
+                this.onDisconnect();
+            })
+            .catch(error => {
+                newData[device.index].isConnecting = false;
+                this.setState({
+                    devicesData: [...newData],
+                });
+                this.alert(error);
+            });
+    };
+
+    /**
+     * 渲染当设备为空时
+     */
+    renderEmptyDevice = () => {
+        return (
+            <Text style={{textAlign: 'center', fontSize: 18, fontWeight: 'bold'}}>
+                没有数据
+            </Text>
+        );
+    };
+
+    /**
+     * 渲染每条蓝牙设备
+     */
+    renderDeviceItem = (device) => {
+        const data = device.item;
+        return (
+            <TouchableOpacity
+                activeOpacity={0.7}
+                disabled={!!this.state.isConnected}
+                style={styles.item}
+                onPress={() => this.connect(device)}
+            >
+                <View style={{flexDirection: 'row'}}>
+                    <Text style={{color: 'black'}}>{data.name ? data.name : 'Unknown device'}</Text>
+                    <Text style={{color: 'red', marginLeft: 50}}>{data.isConnecting ? '连接中...' : ''}</Text>
+                </View>
+                <Text>{data.id}</Text>
+            </TouchableOpacity>
+        );
+    };
+
+    /**
+     * 监听蓝牙断开
+     */
+    onDisconnect = () => {
+        this.disconnectListener = BluetoothManager.manager.onDeviceDisconnected(BluetoothManager.peripheralId, (error, device) => {
+            if (error) {
+                this.setState({
+                    devicesData: [...this.deviceMap.values()],
+                    isConnected: false,
+                });
+            } else {
+                this.disconnectListener && this.disconnectListener.remove();
+            }
+        });
+    };
+
+    write = (index, type) => {
+        if (this.state.text.length === 0) {
+            this.alert('请输入消息');
+            return;
+        }
+        BluetoothManager.write(this.state.text, index)
+            .then(() => {
+                this.bluetoothReceiveData = [];
+                this.setState({
+                    writeData: this.state.text,
+                    text: '',
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
+    writeWithoutResponse = (index, type) => {
+        if (this.state.text.length === 0) {
+            this.alert('请输入消息');
+            return;
+        }
+        BluetoothManager.writeWithoutResponse(this.state.text, index, type)
+            .then(() => {
+                this.bluetoothReceiveData = [];
+                this.setState({
+                    writeData: this.state.text,
+                    text: '',
+                });
+            })
+            .catch(error => {
+                console.log();
+            });
+    };
+
+    read = (index) => {
+        BluetoothManager.read(index)
+            .then(value => {
+                this.setState({readData: value});
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
+    monitor = (index) => {
+        let transactionId = 'monitor';
+        this.monitorListener = BluetoothManager.manager.monitorCharacteristicForDevice(
+            BluetoothManager.peripheralId,
+            BluetoothManager.nofityServiceUUID[index],
+            BluetoothManager.nofityCharacteristicUUID[index],
+            (error, characteristic) => {
+                if (error) {
+                    this.setState({isMonitoring: false});
+                    this.alert('monitor fail: ' + error.reason);
+                } else {
+                    this.setState({isMonitoring: true});
+                    this.bluetoothReceiveData.push(characteristic.value);
+                    this.setState({
+                        receiveData: this.bluetoothReceiveData.join(''),
+                    });
+                }
+            }, transactionId,
+        );
+    };
+
+    renderFooter = () => {
+        return (
+            <View style={{marginBottom: 30}}>
+                {
+                    this.state.isConnected ?
+                        <View>
+                            {
+                                this.renderWriteView(
+                                    '写数据(write)',
+                                    '发送',
+                                    BluetoothManager.writeWithResponseCharacteristicUUID,
+                                    this.write,
+                                )
+                            }
+                            {
+                                this.renderWriteView(
+                                    '写数据(writeWithoutResponse)',
+                                    '发送',
+                                    BluetoothManager.writeWithoutResponseCharacteristicUUID,
+                                    this.writeWithoutResponse,
+                                )
+                            }
+                            {
+                                this.renderReceiveView(
+                                    '读取的数据',
+                                    '读取',
+                                    BluetoothManager.readCharacteristicUUID,
+                                    this.read,
+                                    this.state.readData,
+                                )
+                            }
+                            {
+                                this.renderReceiveView(
+                                    `监听接收的数据：${this.state.isMonitoring ? '监听已开启' : '监听未开启'}`,
+                                    '开启监听',
+                                    BluetoothManager.nofityCharacteristicUUID,
+                                    this.monitor,
+                                    this.state.receiveData)
+                            }
+                        </View>
+                        :
+                        <View style={{marginBottom: 20}}/>
+                }
+            </View>
+        );
+    };
+
+
+    renderWriteView = (label, buttonText, characteristics, onPress, state) => {
+        if (characteristics.length === 0) {
+            return null;
+        }
+        return (
+            <View style={{marginHorizontal: 10, marginTop: 30}} behavior='padding'>
+                <Text style={{color: 'black'}}>{label}</Text>
+                <Text style={styles.content}>
+                    {this.state.writeData}
+                </Text>
+                {characteristics.map((item, index) => {
+                    return (
+                        <TouchableOpacity
+                            key={index}
+                            activeOpacity={0.7}
+                            style={styles.buttonView}
+                            onPress={() => {
+                                onPress(index);
+                            }}>
+                            <Text style={styles.buttonText}>{buttonText} ({item})</Text>
+                        </TouchableOpacity>
+                    );
+                })}
+                <TextInput
+                    style={[styles.textInput]}
+                    value={this.state.text}
+                    placeholder='请输入消息'
+                    onChangeText={(text) => {
+                        this.setState({text});
+                    }}
+                />
+            </View>
+        );
+    };
+    renderReceiveView = (label, buttonText, characteristics, onPress, state) => {
+        if (characteristics.length === 0) {
+            return null;
+        }
+        return (
+            <View style={{marginHorizontal: 10, marginTop: 30}}>
+                <Text style={{color: 'black', marginTop: 5}}>{label}</Text>
+                <Text style={styles.content}>
+                    {state}
+                </Text>
+                {
+                    characteristics.map((item, index) => {
+                        return (
+                            <TouchableOpacity
+                                activeOpacity={0.7}
+                                style={styles.buttonView}
+                                onPress={() => {
+                                    onPress(index);
+                                }}
+                                key={index}>
+                                <Text style={styles.buttonText}>{buttonText} ({item})</Text>
+                            </TouchableOpacity>
+                        );
+                    })
+                }
+            </View>
+        );
     };
 
     render() {
+        const {devicesData, isConnected, text, receiveData, readData, writeData, isMonitoring, scaning} = this.state;
         return (
-            <View>
-                <TouchableOpacity
-                    activeOpacity={0.7}
-                    style={[styles.buttonView, {marginHorizontal: 10, height: 40, alignItems: 'center'}]}
-                    onPress={this.scan.bind(this)}
-                >
-                    <Text style={styles.buttonText}>搜索蓝牙</Text>
-                </TouchableOpacity>
+            <View style={styles.container}>
+                <View style={{marginTop: 20}}>
+                    <TouchableOpacity
+                        activeOpacity={0.7}
+                        style={[styles.buttonView, {marginHorizontal: 10, height: 40, alignItems: 'center'}]}
+                        onPress={isConnected ? this.disconnect.bind(this) : this.scan.bind(this)}>
+                        <Text
+                            style={styles.buttonText}>{scaning ? '正在搜索中...' : isConnected ? '断开蓝牙' : '搜索蓝牙'}</Text>
+                    </TouchableOpacity>
+
+                    <Text style={{marginLeft: 10, marginTop: 10}}>
+                        {isConnected ? '当前连接的设备' : '可用设备'}
+                    </Text>
+                </View>
+
+                <FlatList
+                    data={devicesData}
+                    keyExtractor={item => item.id}
+                    extraData={[isConnected, text, receiveData, readData, writeData, isMonitoring, scaning]}
+                    renderItem={(item) => this.renderDeviceItem(item)}
+                    ListEmptyComponent={this.renderEmptyDevice()}
+                    ListFooterComponent={this.renderFooter()}
+                />
+
             </View>
         );
     }
